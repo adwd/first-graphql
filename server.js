fae12b1fafff02ff031fe1dcf1c16524442128d2
adwd <ecdt.mn@gmail.com>
2017-11-02 22:59:00 +0900

    graphql server

--- server.js
+++ server.js
@@ -1,31 +1,36 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const { graphql, buildSchema } = require('graphql');
 
+// Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
+  type Query {
+    user(max: Int = 10): [User]
+    hello: String
+  }
   type User {
     name: String
     address: String
   }
-  type Query {
-    user(max: Int = 10): [User]
-  }
 `);
 
+// The root provides a resolver function for each API endpoint
 const root = {
   user: (...args) => {
-    console.log(args);
+    // args.forEach(console.log);
+    console.log(args.length);
     return [];
+  },
+  hello: () => {
+    return 'Hello World!';
   },
 };
 
-const query = `
-  {
-    user {
-      name
-    }
-  }
-`
-
-graphql(schema, query, root)
-  .then(response => {
-    console.log(response);
-  });
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
