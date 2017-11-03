fe6ba067e98693d6338bc7fc2e43a5b3ce931817
adwd <ecdt.mn@gmail.com>
2017-11-03 12:14:43 +0900

    mutation

--- server.js
+++ server.js
@@ -4,13 +4,34 @@ const { graphql, buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
+  # input types
+  # Input types can't have fields that are other objects,
+  # only basic scalar types, list types, and other input types.
+  input MessageInput {
+    content: String
+    author: String
+  }
+
   type Query {
     user(max: Int = 10): [User]
     hello: String
     getDie(numSides: Int): RandomDie
     quoteOfTheDay: String
     random: Float!
+    getMessage(id: ID!): Message
   }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
   type User {
     name: String
     address: String
@@ -21,6 +42,15 @@ const schema = buildSchema(`
     roll(numRolls: Int!): [Int]
   }
 `);
+
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
 
 class RandomDie {
   constructor(numSides) {
@@ -36,8 +66,31 @@ class RandomDie {
   }
 }
 
+let fakeDatabase = {};
+
 // The root provides a resolver function for each API endpoint
 const root = {
+  getMessage: ({ id }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: ( {input }) => {
+    // Create a random id for our "database".
+    var id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: ({ id, input }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
   user: (...args) => {
     // args.forEach(console.log);
     console.log(args.length);
