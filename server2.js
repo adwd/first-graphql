54b66bc0b7c911a489e1961344839c8990fd913e
adwd <ecdt.mn@gmail.com>
2017-11-03 13:10:56 +0900

    contructing types

---
+++ server2.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const graphql = require('graphql');
+
+// Maps id to User object
+let fakeDatabase = {
+  'a': {
+    id: 'a',
+    name: 'alice',
+  },
+  'b': {
+    id: 'b',
+    name: 'bob',
+  },
+};
+
+// Define the User type
+const userType = new graphql.GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: graphql.GraphQLString },
+    name: { type: graphql.GraphQLString },
+  }
+});
+
+const queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    user: {
+      type: userType,
+      // `args` describes the arguments that the `user` query accepts
+      args: {
+        id: { type: graphql.GraphQLString }
+      },
+      resolve: (_, { id }) => {
+        return fakeDatabase[id];
+      }
+    }
+  }
+});
+
+const schema = new graphql.GraphQLSchema({ query: queryType });
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
